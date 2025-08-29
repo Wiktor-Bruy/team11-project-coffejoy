9964a4b76c7f8b17689a58debfb061f0613d779e
Crazy_Fox <holmyliszag@ukr.net>
2025-08-29 14:12:06 +0300

    script

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,17 @@
-console.log('menu');
+(() => {
+  const refs = {
+    //Додавання атрибута на кнопку відкриття меню
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    //Додавання атрибута на кнопку закриття меню
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    //Додавання атрибута на бекдроп меню
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', togMenu);
+  refs.closeMenuBtn.addEventListener('click', togMenu);
+
+  function togMenu() {
+    refs.menu.classList.toggle('is-open');
+  }
+})();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,17 @@
-console.log('modal');
+(() => {
+  const refs = {
+    //Додавання атрибута на кнопку відкриття модалки
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    //Додавання атрибута на кнопку закриття модалки
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    //Додавання атрибута на бекдроп модалки
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', togModal);
+  refs.closeModalBtn.addEventListener('click', togModal);
+
+  function togModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
